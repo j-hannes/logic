250db7efa000027e4d0e372ffe724bd755fd73c3
Jay Hannes <Hannes_E@gmx.de>
2014-06-15 15:17:07 +0200

    Put set in separate folder

--- src/js/application.js
+++ src/js/application.js
@@ -2,7 +2,7 @@ var Board = require('./modules/board/board')
 var Grid = require('./modules/grid/grid')
 var GridView = require('./modules/grid/grid-view')
 
-var set = require('./set')
+var set = require('./sets/set-9')
 
 var board = new Board(set)
 var grid = new Grid(board)
