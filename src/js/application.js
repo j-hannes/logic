89318ea778c749e05481b026773fc6be42cb0612
Jay Hannes <Hannes_E@gmx.de>
2014-06-15 15:20:23 +0200

    Create a view for the board

--- src/js/application.js
+++ src/js/application.js
@@ -1,9 +1,13 @@
-var Board = require('./modules/board/board')
-var Grid = require('./modules/grid/grid')
-var GridView = require('./modules/grid/grid-view')
+var Board     = require('./modules/board/board')
+var BoardView = require('./modules/board/board-view')
+var Grid      = require('./modules/grid/grid')
+var GridView  = require('./modules/grid/grid-view')
 
 var set = require('./sets/set-9')
 
 var board = new Board(set)
 var grid = new Grid(board)
-new GridView({model: grid})
+
+var gridView = new GridView({model: grid})
+var boardView = new BoardView({model: board})
+boardView.renderToGrid(gridView)
---
+++ src/js/modules/board/board-view.js
@@ -0,0 +1,12 @@
+var $ = require('jquery')
+var Backbone = require('backbone')
+Backbone.$ = $
+// var _ = require('underscore')
+
+var BoardView = Backbone.View.extend({
+  renderToGrid: function() {
+    
+  }
+})
+
+module.exports = BoardView
