0943c845d64c3a8c6c32cc0de4a0c333f1aae85a
Jay Hannes <Hannes_E@gmx.de>
2014-06-27 10:21:24 +0200

    Stage 11

--- src/js/application.js
+++ src/js/application.js
@@ -67,19 +67,6 @@ var Row = Backbone.Model.extend({
 
 var RowCollection = Backbone.Collection.extend({
   model: Row,
-
-  offset: function() {
-    var blocks = this.pluck('blocks')
-    var comparator = function(block) {return block.length}
-    var spaceForBlocks =_.max(blocks, comparator).length
-    var spaceForMarker = 1
-    var spaceForSum = 1
-    return spaceForBlocks + spaceForMarker + spaceForSum
-  },
-
-  totalLength: function() {
-    return this.offset() + this.at(0).get('cells').length
-  },
 })
 
 var Board = Backbone.Model.extend({
@@ -100,7 +87,9 @@ var Board = Backbone.Model.extend({
   createRowModelData: function(cells) {
     return function(blocks, rowId) {
       return {
-        blocks: blocks,
+        blocks: new BlockCollection(_.map(blocks, function(block) {
+          return {value: block}
+        })),
         cells: new CellCollection(cells[rowId]),
       }
     }
@@ -121,14 +110,144 @@ var Board = Backbone.Model.extend({
 
 // VIEW LAYER
 
-var RowView = Backbone.View.extend({
+var MarkerView = Backbone.View.extend({
+  className: 'marker',
+
+  events: {
+    'click': 'toggleMark',
+  },
+
+  toggleMark: function() {
+    this.$el.toggleClass('marked')
+  },
+
+  render: function(coord) {
+    var target = $('*[data-coord="' + coord.x + ',' + coord.y + '"]')
+    // this.$el.text('M') // FIXME devtrace
+    this.$el.appendTo(target)
+  }
 })
 
+var OverlapView = Backbone.View.extend({
+  className: 'overlap',
+
+  render: function(coord) {
+    var target = $('*[data-coord="' + coord.x + ',' + coord.y + '"]')
+    // this.$el.text('O') // FIXME devtrace
+    this.$el.appendTo(target)
+  }
+})
+
+var BlockView = Backbone.View.extend({
+  className: 'block',
+
+  render: function(coord) {
+    var target = $('*[data-coord="' + coord.x + ',' + coord.y + '"]')
+    this.$el.text(this.model.get('value'))
+    this.$el.appendTo(target)
+  },
+})
+var BlockCollectionView = Backbone.View.extend({
+  render: function(coordinates) {
+    this.collection.each(function(block, index) {
+      var view = new BlockView({model: block})
+      view.render(coordinates[index])
+    }, this)
+  }
+
+})
+
+// var CellView = Backbone.View.extend({
+//
+// })
+var CellCollectionView = Backbone.View.extend({
+
+})
+
+var RowView = Backbone.View.extend({
+  render: function(options, coordinates) {
+    var markerView = new MarkerView({model: this.model.get('marker')})
+    markerView.render(coordinates.marker)
+
+    var overlapView = new OverlapView({model: this.model.get('overlap')})
+    overlapView.render(coordinates.overlap)
+
+    var blocksView = new BlockCollectionView({
+      collection: this.model.get('blocks')
+    })
+    blocksView.render(coordinates.blocks)
+
+    var cellsView = new CellCollectionView({
+      collection: this.model.get('cells')
+    })
+    // cellsView.render(coordinates.cellsPos)
+  },
+})
+
+
 var RowCollectionView = Backbone.View.extend({
+  getBlockSpace: function() {
+    var blocks = this.collection.pluck('blocks')
+    return _.max(blocks, function(block) {return block.length}).length
+  },
+
+  getOffset: function() {
+    var spaceForMarker = 1
+    var spaceForIndicator = 1
+    return spaceForMarker + spaceForIndicator + this.getBlockSpace()
+  },
+
+  getTotalLength: function() {
+    var amountOfCells = this.collection.at(0).get('cells').length
+    return this.getOffset() + amountOfCells
+  },
+
+  render: function(options) {
+    this.collection.each(function(row, rowId) {
+      var coordinates = this.getCoordinates(rowId)
+      var rowView = new RowView({model: row})
+      rowView.render(options, coordinates)
+    }, this)
+  },
+})
+
+var RowCollectionHorizontalView = RowCollectionView.extend({
+  getCoordinates: function(rowId) {
+    var position = this.offsetY + rowId
+    var blocks = _.map(_.range(this.getBlockSpace()), function(blockPos) {
+      var amountOfBlocks = this.collection.at(rowId).get('blocks').length
+      var buffer = this.getBlockSpace() - amountOfBlocks
+      var x = 2 + buffer + blockPos
+      return {x: x, y: position}
+    }, this)
+    var coords = {
+      marker: {x: 0, y: position},
+      overlap: {x: 1, y: position},
+      blocks: blocks
+    }
+    return coords
+  },
+})
+
+var RowCollectionVerticalView = RowCollectionView.extend({
+  getCoordinates: function(rowId) {
+    var position = this.offsetX + rowId
+    var blocks = _.map(_.range(this.getBlockSpace()), function(blockPos) {
+      var buffer = this.getBlockSpace() - this.collection.at(rowId).get('blocks').length
+      return {x: position, y: 2 + buffer + blockPos}
+    }, this)
+    return {
+      marker: {x: position, y: 0},
+      overlap: {x: position, y: 1},
+      blocks: blocks,
+    }
+  },
 })
 
 var GridView = Backbone.View.extend({
-  template: _.template('<table><tbody></tbody></table>'),
+  tagName: 'table',
+
+  template: _.template('<tbody></tbody>'),
 
   render: function() {
     var rowNumbers = _.range(this.model.get('height'))
@@ -137,10 +256,10 @@ var GridView = Backbone.View.extend({
     this.$el.html(this.template())
     var tbody = this.$('tbody')
 
-    _.each(rowNumbers, function() {
+    _.each(rowNumbers, function(y) {
       var row = $('<tr class="row">')
-      _.each(colNumbers, function() {
-        row.append($('<td class="col">'))
+      _.each(colNumbers, function(x) {
+        row.append($('<td class="col" data-coord="' + x + ','+ y + '">'))
       }, this)
       tbody.append(row)
     }, this)
@@ -159,31 +278,35 @@ var BoardView = Backbone.View.extend({
   el: '#board',
 
   initialize: function(options) {
+    // options.set.horizontal.splice(5,20)
+    // options.set.vertical.splice(5,30)
     this.model = new Board()
     this.model.initializeSet(options.set)
+
+    this.horizontalRowsView = new RowCollectionHorizontalView({
+      collection: this.model.get('hRows'),
+    })
+
+    this.verticalRowsView = new RowCollectionVerticalView({
+      collection: this.model.get('vRows'),
+    })
+
+    // FUCK IT!
+    this.verticalRowsView.offsetX = this.horizontalRowsView.getOffset()
+    this.horizontalRowsView.offsetY = this.verticalRowsView.getOffset()
+
+    var gridModel = new Backbone.Model({
+      width: this.horizontalRowsView.getTotalLength(),
+      height: this.verticalRowsView.getTotalLength(),
+    })
+    this.gridView = new GridView({model: gridModel})
   },
 
   render: function() {
-    this.createGrid()
-
-    var view = new RowCollectionView({collection: this.model.get('hRows')})
-    view.render({
-      gridView: this.gridView,
-      fromRow: 1,
-      withCells: true,
-    })
-
-    // now place content of model in grid (assign grid fields and call render
-    // function of RowView)
-    return this
-  },
-
-  createGrid: function() {
-    this.gridView = new GridView({model: new Backbone.Model({
-      width: this.model.get('hRows').totalLength(),
-      height: this.model.get('vRows').totalLength(),
-    })})
     this.$el.html(this.gridView.render().el)
+    this.horizontalRowsView.render()
+    this.verticalRowsView.render()
+    return this
   },
 })
 
