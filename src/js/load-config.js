a2104954e7696138d780f7ea6d1afa0350cd18ce
Jay Hannes <Hannes_E@gmx.de>
2015-01-22 16:04:34 +0100

    Structure file more clearly

--- src/js/load-config.js
+++ src/js/load-config.js
@@ -3,22 +3,31 @@
 require('./lib/hs').expose(global);
 var fs = require('fs');
 
-//+ a -> String
+//+ toString :: a -> String
 var toString = function(obj) {return obj.toString()};
-//+ FilePath -> String
+
+//+ readFile :: FilePath -> String
 var readFile = compose(toString, fs.readFileSync);
 
-//+ [String] -> Bool
+//+ delimiter :: [String] -> Bool
 var delimiter = contains('');
-//+ [a] -> [a]
+
+//+ getHorizontalBlocks :: [a] -> [a]
 var getHorizontalBlocks = takeWhile(not(delimiter));
-//+ [a] -> [a]
+
+//+ getVerticalBlocks :: [a] -> [a]
 var getVerticalBlocks = compose(
   takeWhile(not(delimiter)),
   tail,
   dropWhile(not(delimiter))
 );
-//+ String -> PuzzleConfig
+
+// type PuzzleConfig = {
+//     horizontalBlocks :: [BlockLengths]
+//   , verticalBlocks :: [BlockLenghts]
+// }
+
+//+ parseConfig :: String -> PuzzleConfig
 var parseConfig = function(configString) {
   var configBlocks = map(words, lines(configString));
   return {
@@ -26,11 +35,9 @@ var parseConfig = function(configString) {
     verticalBlocks: getVerticalBlocks(configBlocks),
   };
 };
-// type PuzzleConfig = {
-//     horizontalBlocks :: [BlockLengths]
-//   , verticalBlocks :: [BlockLenghts]
-// }
 
-//+ FilePath -> PuzzleConfig
-module.exports = compose(parseConfig, readFile);
+//+ loadConfig :: FilePath -> PuzzleConfig
+var loadConfig = compose(parseConfig, readFile);
+
+module.exports = loadConfig
 
